20a8b8a625d95cc802d50cd6a2bfd6d372cf3624
Charif El Belghiti <95632523+C-EB@users.noreply.github.com>
2025-07-30 18:10:21 +0100

    add the projects files

---
+++ config.py
@@ -0,0 +1,11 @@
+jumia_base_url = 'https://www.jumia.com.eg/ar'
+gecko_driver = '/driver/geckodriver'
+headless = False
+
+# https://developers.whatismybrowser.com/useragents/explore/
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:13.5) Gecko/20100101 Firefox/13.5.4'
+page_load_timeout = 30
+
+mongo_url = ''
+mongo_username = ''
+mongo_password = ''
